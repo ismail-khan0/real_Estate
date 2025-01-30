ac7a15e9bf6213f61880b043da783cbca7be5699
ismail-khan0 <ik8323253@gmail.com>
2025-01-30 00:14:12 -0800

    create contact component

---
+++ src/Pages/ContactPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Contact_form from '../Components/Contact_form'
+
+function ContactPage() {
+
+  return (
+    <div className=''>
+      <Contact_form/>
+    </div>
+  )
+}
+
+export default ContactPage
